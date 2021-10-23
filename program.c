eeb96d4ca46ab5dd509ef385f2d96697fc910843
BOSRclub <82073466+BOSRclub@users.noreply.github.com>
2021-10-23 09:55:55 +0545

    Program added
    Another program added

--- program.c
+++ program.c
@@ -1,29 +1,16 @@
-#include<stdio.h>
-int isprime(int n) /*user defined function */
+#include <stdio.h>
+int main()
 {
-if(n==0 || n==1)
-{
-    return 0;
-
-}
-for(int i=2;i<n;i++)
-{
-if(n%i==0) return 0;
-}
-return 1;
-}
-int main() /*main function */
-{
-    int n;
-    printf("Enter the number:");
+    clrscr();
+    int y,n,m,d,j;
+    printf("enter the input");
     scanf("%d",&n);
-    if(isprime(n)==0)
-    {
-        printf("%d is not prime number.",n);
-    }
-    else if(isprime(n)==1)
-    {
-        printf("%d is a prime number.",n);
-    }
-    return 0;
+    y=n/365;
+    m=n%365;
+    j=n/30;
+    d=n%30;
+    printf("year=%d",y);
+    printf("months =%d",j);
+    printf("days=%d",d);
+    getch();
 }
