8ef6e530d250ac795c1019ff13f93f26e94c01c6
BOSRclub <82073466+BOSRclub@users.noreply.github.com>
2021-10-23 09:53:43 +0545

    Program added
    Another program

--- program.c
+++ program.c
@@ -1,17 +1,29 @@
-#include <stdio.h>
-int main()
+#include<stdio.h>
+int isprime(int n) /*user defined function */
 {
-    int a,b,c;
-    printf("enter a,b,c");
-    scanf("%d%d%d",&a,&b,&c);
-   if(a>b)
-   {
-       if(a>c) printf("a is larger");
-       else printf("c is larger");
-   }
-   else
-   {
-       if(b>c) printf("b is larger");
-       else printf("c is larger");
-   }
+if(n==0 || n==1)
+{
+    return 0;
+
+}
+for(int i=2;i<n;i++)
+{
+if(n%i==0) return 0;
+}
+return 1;
+}
+int main() /*main function */
+{
+    int n;
+    printf("Enter the number:");
+    scanf("%d",&n);
+    if(isprime(n)==0)
+    {
+        printf("%d is not prime number.",n);
+    }
+    else if(isprime(n)==1)
+    {
+        printf("%d is a prime number.",n);
+    }
+    return 0;
 }
