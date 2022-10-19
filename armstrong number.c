874ee4cdb1eeb3963b404eb5056c10409f1ec4dc
Abhishek Kumar <www.abhishek090300@gmail.com>
2022-10-19 12:58:29 +0530

    readable more

--- armstrong number.c
+++ armstrong number.c
@@ -4,28 +4,28 @@
 
 int main()
  {
-   int num, originalNum, remainder, n = 0;
+   int num,  n = 0;
    float result = 0.0;
 
-   printf("Enter an integer: ");
+   printf("Enter an number: ");
    scanf("%d", &num);
 
-   originalNum = num;
+   int originalNum = num;
 
    // store the number of digits of num in n
    for (originalNum = num; originalNum != 0; ++n) {
-       originalNum /= 10;
+       originalNum= originalNum/10;
    }
-
+    int remainder;
    for (originalNum = num; originalNum != 0; originalNum /= 10) {
        remainder = originalNum % 10;
 
       // store the sum of the power of individual digits in result
-      result += pow(remainder, n);
+      result = result+pow(remainder, n);
    }
-
+    int k=result
    // if num is equal to result, the number is an Armstrong number
-   if ((int)result == num)
+   if (k== num)
     printf("%d is an Armstrong number.", num);
    else
     printf("%d is not an Armstrong number.", num);
